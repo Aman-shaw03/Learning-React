06a882822d442883c013d9bf3fc7ece2c44a8e29
Aman shaw <shaw08122@gmail.com>
2024-06-09 12:24:01 +0530

    learning React Day 01

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+
+
+  return (
+    <>
+    <Chai/>
+      <h1>Chai is Ready</h1>
+      <p>50 rs saar</p>
+    </>
+  )
+}
+
+export default App
---
+++ learnings/01_basics.js
@@ -0,0 +1,10 @@
+/* Notes from Learning React */
+
+/*React is a framework but it also has many libraries like vite , vanillajs , createReact */
+/*framework is military and library is cool dude */
+
+//  The most important thing is "src" folder in a react app . it holds our index.js and app.js file but our index.html file could be separately in public folder or out of it .The
+
+//  usually our React app is SPA (single page application) since it holds only 1 div and through jsx we inject our html
+
+//  while writing our function inisde our jsx file and also naming of our jsx file , start with capitalize letter like chai.jsx is wrong it should be Chai.jsx function Chai(){} and the format of our inject file should jsx(standard) not js , we should also export and import our function to inject
