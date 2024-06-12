c546e2f355ed7c0b64ed2f8297baf913075d4938
Aman shaw <shaw08122@gmail.com>
2024-06-12 15:24:34 +0530

    create a card with using props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,34 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "chai_Biscuit",
+    age: 24
+  }
+  let myArr = [5,8,9]
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-2xl'>Tailwind Test</h1>
 
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/13172901/pexels-photo-13172901.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width="384" height="512"/>
-  <div class="pt-6 space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 ">
-        Sarah Dayan
-      </div>
-      <div>
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+      <Card channel = "ToastyBoy" btntext={"Kothibanglacheck"} />
+      <Card channel = "Margaraite" />
+
     </>
   )
 }
---
+++ learnings/03_basics.js
@@ -0,0 +1,6 @@
+/* Today i learn how to install tailwind with its dev dependencies  */
+/* props = when we create a function for any element(card, button) in react it accept a props(properties)
+which we can use to pass details , so it would take lesser time to create same type of elements 
+or simply pass {channel,btnText} and use them  */
+
+/*ALso explore devUI.io */
