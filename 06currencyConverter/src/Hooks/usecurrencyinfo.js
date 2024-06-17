4bfbe637539dd4c4971c0924a830e0d21c0f8c36
Aman shaw <shaw08122@gmail.com>
2024-06-17 12:12:04 +0530

    .

--- 06currencyConverter/src/Hooks/usecurrencyinfo.js
+++ 06currencyConverter/src/Hooks/usecurrencyinfo.js
@@ -10,8 +10,9 @@ import {useEffect, useState} from "React"
 //  }
 
 function useCurrencyInfo(currency){
-    useEffect = ( ()=> {
     const [data, setData] = useState({})
+    
+    useEffect = ( ()=> {
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
     .then((res)=>res.json())
     .then((res)=>setData(res[currency]))
