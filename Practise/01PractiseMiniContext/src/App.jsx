096147603cd9bb4fc39ecd1020a035e0e4759555
Aman shaw <shaw08122@gmail.com>
2024-06-26 15:30:13 +0530

    themeswitcher project ongoing

--- Practise/01PractiseMiniContext/src/App.jsx
+++ Practise/01PractiseMiniContext/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserContextProvider from '../src/Context/UserContextProvider'
+import LogIn from './Components/LogIn'
+import Message from './Components/Message'
+
 
 function App() {
 
@@ -10,8 +13,8 @@ function App() {
   return (
     <UserContextProvider>
       <h1>React winning</h1>
-      <LogIn/>
-      <Message/>
+      <LogIn />
+      <Message />
     </UserContextProvider>
   )
 }
