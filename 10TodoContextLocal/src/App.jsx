fea3a9ae93eec66d34c208522637b09a170a0999
Aman shaw <shaw08122@gmail.com>
2024-06-28 14:50:07 +0530

    Done with todo with Local storage and Context API

--- 10TodoContextLocal/src/App.jsx
+++ 10TodoContextLocal/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodo] = useState([])
@@ -18,7 +20,7 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodo((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo))
+    setTodo((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo))
   }
 
   useEffect(() => {
@@ -40,9 +42,18 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div 
+                          key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
