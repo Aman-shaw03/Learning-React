50e002c86b7c2c136913452d1878817359a6da5f
Aman shaw <shaw08122@gmail.com>
2024-07-07 14:16:21 +0530

    MegaBlog RTE component

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -12,7 +12,42 @@ export default function RTE({name, label, control, defaultValue = ""}) {
         name={name || "content"}
         control={control}
         // whatever parent form/component will give control , it will take that
-        render={() => ()}
+        render={({field: {onChange}}) => (
+            <Editor
+                initialValue={defaultValue}
+            init={{
+                initialValue: defaultValue,
+                height: 500,
+                menubar: true,
+                plugins: [
+                    "image",
+                    "advlist",
+                    "autolink",
+                    "lists",
+                    "link",
+                    "image",
+                    "charmap",
+                    "preview",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "code",
+                    "help",
+                    "wordcount",
+                    "anchor",
+                ],
+                toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+            onEditorChange={onChange}
+            />
+        )}
         />
     </div>
   )
