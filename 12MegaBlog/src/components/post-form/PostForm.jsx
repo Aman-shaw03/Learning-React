6937cfb042ab2e5700c0299486a2079cd91882e4
Aman shaw <shaw08122@gmail.com>
2024-07-08 23:41:10 +0530

    postForm

--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -36,14 +36,98 @@ function PostForm({post}) {
       
     
     } else{
-      
+      const file = await appwriteService.uploadFile(data.image[0]);
+
+      if(file){
+        const fileId = file.$id
+        data.featureImage = fileId
+        const dbPost = await appwriteService.createPost({
+          ...data,
+          userId: userData.$id
+        })
+        if(dbPost){
+          navigate(`/post/${dbPost.$id}`)
+        }
+      }
     }
   }
 
+  const slugTransform = useCallback((value) =>{
+    if(value && typeof value === "string"){
+      return value
+      .trim()
+      .toLowerCase()
+      .replace(/[a-zA-Z\d\s]+/g,"-")
+      .replace(/[\s]/g,"-")
+    }
+  },[])
 
-  return (
-    <div></div>
-  )
+    React.useEffect(() => {
+      const subscription = watch( (value , {name}) => {
+        if(name === "title"){
+          setValue("slug", slugTransform(value.title, {shouldValidate: true}))
+        }
+      })
+      // interview Q :- in this useEffect we are calling a method ...how can we optimise it ?
+      // we can hold the method in a variable and then at the end return the variable that hold the function and Unsubscribe 
+      // so it wont keep it in a loop , that way we can optimise it
+
+      return () =>{
+        subscription.unsubscribe()
+      }
+
+
+    },[slugTransform, watch , setValue])
+
+    return (
+      <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+          <div className="w-2/3 px-2">
+              <Input
+                  label="Title :"
+                  placeholder="Title"
+                  className="mb-4"
+                  {...register("title", { required: true })}
+              />
+              <Input
+                  label="Slug :"
+                  placeholder="Slug"
+                  className="mb-4"
+                  {...register("slug", { required: true })}
+                  onInput={(e) => {
+                      setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                  }}
+              />
+              <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+          </div>
+          <div className="w-1/3 px-2">
+              <Input
+                  label="Featured Image :"
+                  type="file"
+                  className="mb-4"
+                  accept="image/png, image/jpg, image/jpeg, image/gif"
+                  {...register("image", { required: !post })}
+              />
+              {post && (
+                  <div className="w-full mb-4">
+                      <img
+                          src={appwriteService.getFilePreview(post.featuredImage)}
+                          alt={post.title}
+                          className="rounded-lg"
+                      />
+                  </div>
+              )}
+              <Select
+                  options={["active", "inactive"]}
+                  label="Status"
+                  className="mb-4"
+                  {...register("status", { required: true })}
+              />
+              <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                  {post ? "Update" : "Submit"}
+              </Button>
+          </div>
+      </form>
+  );
 }
 
 export default PostForm
