c3743e2374b7a9ff32046dd126f89152f87d1435
Aman shaw <shaw08122@gmail.com>
2024-07-09 23:18:39 +0530

    Post Page

--- 12MegaBlog/src/pages/Home.jsx
+++ 12MegaBlog/src/pages/Home.jsx
@@ -12,8 +12,36 @@ function Home() {
             }
         })
     },[])
+
+    if(posts){
+      return(
+        <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+      )
+    }
+
+
   return (
-    <div>Home</div>
+    <div className='w-full py-8'>
+      <Container>
+        <div className='flex flex-wrap'>
+          {posts.map((post) => (
+            <div key={post.$id} className='p-2 w-1/4'>
+              <PostCard {...post}/>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
   )
 }
 
