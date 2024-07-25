683462877297ae9b7aac9d595f590c8c1a5ed1dc
Aman shaw <shaw08122@gmail.com>
2024-07-25 14:56:22 +0530

    .

--- 06currencyConverter/src/Hooks/usecurrencyinfo.js
+++ 06currencyConverter/src/Hooks/usecurrencyinfo.js
@@ -15,7 +15,6 @@ function useCurrencyInfo(currency){
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
     .then((res)=>res.json())
     .then((res)=>setData(res[currency]))
-    console.log(data);
     },[currency])
 
     console.log(data);
