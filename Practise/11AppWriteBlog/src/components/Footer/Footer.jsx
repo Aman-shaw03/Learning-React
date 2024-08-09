609242c76114babe94ee3ef6f7741b0ea0983603
Aman shaw <shaw08122@gmail.com>
2024-08-09 15:15:54 +0530

    setting Header

--- Practise/11AppWriteBlog/src/components/Footer/Footer.jsx
+++ Practise/11AppWriteBlog/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../Logo'
+import Logo from "../index"
 import {Link} from "react-router-dom"
 
 function Footer() {
--- Practise/11AppWriteBlog/src/components/Header/Header.jsx
+++ Practise/11AppWriteBlog/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-
+import {Logo, LogoutBtn, Container } from "../index"
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 function Header() {
+  const authStatus = useSelector( state => state.auth.status)
+  const navigate = useNavigate()
   return (
     <div>Header</div>
   )
