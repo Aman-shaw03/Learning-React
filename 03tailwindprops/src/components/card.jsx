7fd382d0d40e8a85527889e6e0e9c3143388b883
Aman shaw <shaw08122@gmail.com>
2024-10-24 15:34:32 +0530

    .

--- 03tailwindprops/src/components/card.jsx
+++ 03tailwindprops/src/components/card.jsx
@@ -6,15 +6,15 @@ function Card({channel,btntext = "AMeer"}) {
     return (
         // console.log(props.channel),
         // console.log(props),
-        <div className="flex max-w-2xl flex-col items-center rounded-md border md:flex-row mb-9 mt-6">
+      <div className="flex max-w-2xl flex-col items-center rounded-md border md:flex-row mb-9 mt-6">
         <div className="h-full w-full md:h-[200px] md:w-[300px]">
-    <img
-      src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-      alt="Laptop"
-      className="h-full w-full rounded-md object-cover"
-    />
-  </div>
-  <div>
+          <img
+            src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+            alt="Laptop"
+            className="h-full w-full rounded-md object-cover"
+          />
+        </div>
+      <div>
     <div className="p-4">
       <h1 className="inline-flex items-center text-lg font-semibold">
         {channel}{" "}
--- revise/01Practise/src/App.jsx
+++ revise/01Practise/src/App.jsx
@@ -8,26 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-gray-700'>
+        <div className='bg-gray-800 text-white'>
+          <h1>React Revision</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
